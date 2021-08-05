781923bd6ab49905f6c0b07bf432dd5376c5bf21
OmarFattahi <88429089+OmarFattahi@users.noreply.github.com>
2021-08-05 23:47:29 +0430

    Page Login

---
+++ Page Login.py
@@ -0,0 +1,17 @@
+print("this is a test program\n\n")
+
+username = input("UserName:")
+password = input("\nPassCode:")
+
+username = str(username)
+password = str(password)
+
+if username == 'omar':
+	if password == '1':
+		print("\nwelcome to your program")
+		
+	else:
+		print("\nUsername Or Passcode Invalid ^_^ 	\n\n\n\n\nProgrammer: Mister Deoder")
+		
+else:
+	print("\nUsername Or Passcode Invalid ^_^ 	\n\n\n\n\nProgrammer: Mister Deoder")
