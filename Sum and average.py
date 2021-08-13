22ac8ea1267b82538ceb87d2f2539d5e6b56d557
OmarFattahi <88429089+OmarFattahi@users.noreply.github.com>
2021-08-13 15:23:06 +0430

    Delete Sum and average.py

--- Sum and average.py
+++
@@ -1,9 +0,0 @@
-a = input("avali:")
-a = float(a)
-b = input("dovomi:")
-b = float(b)
-c = input("sevomi:")
-c = float(c)
-sum = a + b + c
-ave = float(sum) / 3.0
-print(ave ,"\n\n\n\nProgrammer:MisterCoder")
