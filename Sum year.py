2bf2c6843c4400d997c6a395df025528ad12f1cb
OmarFattahi <88429089+OmarFattahi@users.noreply.github.com>
2021-08-13 15:23:15 +0430

    Delete Sum year.py

--- Sum year.py
+++
@@ -1,8 +0,0 @@
-a = int(input("YERS:"))
-b = int(input("MONTH:"))
-c = int(input("DAY:"))
-berooz = a * 365 + b * 30 + c
-print("\n\tBe rooz:", berooz,"(D)")
-print("--------------------------------------")
-besaat = a * 365 * 24 + b * 30 * 24 + c * 24
-print("\tBe saat:", besaat, "(H)")
